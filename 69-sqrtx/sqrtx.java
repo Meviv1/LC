d7031963247ca46e00cd78b85f6cd34d72c12a87
Meviv1 <131643604+Meviv1@users.noreply.github.com>
2025-08-21 08:26:49 +0530

    Time: 1 ms (84.32%) | Memory: 41 MB (40.99%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+       if (x == 0 || x==1 )
+            return x;
+        int st = 1;
+        int en = x;
+        while (st <= en) {
+            int mid = st + (en - st) / 2;
+            if (mid==x/mid)
+                return mid;
+            else if (mid>x/mid) {
+                en = mid - 1;
+            }
+            else {
+                st = mid + 1;
+            }
+        }
+        return en;  
+    }
+}
